870ce047d95f97024ed565103a9d6a03f3e9d284
MAlicata <matiasalicata@gmail.com>
2022-11-20 01:13:42 -0300

    Initial commit

---
+++ src/CocheCRUDImpl.java
@@ -0,0 +1,16 @@
+public class CocheCRUDImpl implements CocheCRUD{
+    @Override
+    public void save() {
+        System.out.println("Method save()");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("Method findAll()");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Method delete()");
+    }
+}
